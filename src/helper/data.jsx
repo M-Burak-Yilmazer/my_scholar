252245c644b1028c0a76528819f31ab69a212dd1
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2023-12-11 19:55:54 +0300

    first commit

---
+++ src/helper/data.jsx
@@ -0,0 +1,52 @@
+export const appointmentData = [];
+
+export const doctorData = [
+  {
+    id: 1,
+    name: "Barış Akıncı",
+    dep: "Fizik",
+    img: "./img/dr1.png",
+  },
+  {
+    id: 2,
+    name: "Bayram Meral",
+    dep: "Coğrafya",
+    img: "./img/dr2.png",
+  },
+  {
+    id: 3,
+    name: "Can Koni",
+    dep: "Eğitim Bilimleri",
+    img: "./img/dr3.png",
+  },
+  {
+    id: 4,
+    name: "Dilek Kuvvet ",
+    dep: "Gastroentogoly",
+    img: "./img/dr4.png",
+  },
+  {
+    id: 5,
+    name: "Ebru Tuncay Yıldız   ",
+    dep: "Geometri",
+    img: "./img/dr5.png",
+  },
+  {
+    id: 6,
+    name: "İlyas Güneş",
+    dep: "Matematik",
+    img: "./img/dr6.png",
+  },
+  {
+    id: 7,
+    name: "Kadir Gümüş",
+    dep: "Edebiyat",
+    img: "./img/dr7.png",
+  },
+  {
+    id: 8,
+    name: "Ramazan Yetkin",
+    dep: "Tarih",
+    img: "./img/dr8.png",
+  },
+];
